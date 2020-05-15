d6a1fb6f97a693ef6aec7f492fbd6fa2b0b073c1
MinexAutomation <minex.automation@gmail.com>
2020-05-15 00:41:36 -0700

    Added Base library with IProcessStartTimeProvider service definition interface.

---
+++ source/R5T.D0003.Base/Code/Services/Definitions/IProcessStartTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0003
+{
+    public interface IProcessStartTimeProvider
+    {
+        Task<DateTime> GetProcessStartTimeAsync();
+    }
+}
