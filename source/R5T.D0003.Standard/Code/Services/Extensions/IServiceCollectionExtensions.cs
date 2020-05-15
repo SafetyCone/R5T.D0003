4a91b4b806ae6daefffa0002336079520b9fe747
MinexAutomation <minex.automation@gmail.com>
2020-05-15 01:07:48 -0700

    Added the R5T.D0003.Standard standard aggregation library for the IProcessStartTimeProvider service.

---
+++ source/R5T.D0003.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.D0003.Default;
+
+
+namespace R5T.D0003.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IProcessStartTimeProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddProcessStartTimeProvider(this IServiceCollection services)
+        {
+            services.AddDefaultProcessStartTimeProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IProcessStartTimeProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IProcessStartTimeProvider> AddProcessStartTimeProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<IProcessStartTimeProvider>.New(() => services.AddProcessStartTimeProvider());
+            return serviceAction;
+        }
+    }
+}
